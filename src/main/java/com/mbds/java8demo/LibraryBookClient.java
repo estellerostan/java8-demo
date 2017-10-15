7539e6cee74222e54c9014d3fbf8020eed1238c6
Estelle Rostan <estelle.rostan@etu.unice.fr>
2017-10-15 16:34:00 +0200

    Create a test request
    Returning http status code OK

--- src/main/java/com/mbds/java8demo/LibraryBookClient.java
+++ src/main/java/com/mbds/java8demo/LibraryBookClient.java
@@ -1,5 +1,19 @@
 package com.mbds.java8demo;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 public class LibraryBookClient {
+
+	public static void main(String[] args) throws UnirestException {
+
+		HttpResponse<JsonNode> jsonResponse = Unirest.get("http://localhost:8080/bibliothequelivreservice/api/livre/1")
+				.header("accept", "application/json")
+				.asJson();
+		
+		System.out.println(jsonResponse.getStatus());
+	}
 
 }
